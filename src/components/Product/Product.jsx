bebcba89ebb6a8a7af607ad1879e50103dbbfd7f
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 23:43:57 +0100

    feat: add price and add to cart button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,12 +3,14 @@ import styles from "./Product.module.scss";
 
 export default class Product extends Component {
   render() { 
-    const { name, img } = this.props.product;
+    const { name, price, img } = this.props.product;
     
     return (
       <div className={styles.product}>
         <img src={img} alt={name}/>
         <h2>{name}</h2>
+        <h3>£{price}</h3>
+        <button>Add to Cart</button>
       </div>
     );
   }
