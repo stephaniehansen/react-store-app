72190415ce80838f806648cf2a62c80ccedf578e
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 21:40:46 +0100

    chore: add sample data

---
+++ src/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    name: "Product 1",
+    price: 9.99,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
+    img: "https://picsum.photos/200/300"
+  },
+  {
+    name: "Product 2",
+    price: 9.99,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
+    img: "https://picsum.photos/200/300"
+  },
+  {
+    name: "Product 3",
+    price: 9.99,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
+    img: "https://picsum.photos/200/300"
+  },
+  {
+    name: "Product 4",
+    price: 9.99,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
+    img: "https://picsum.photos/200/300"
+  },
+]
+
+export default products;
