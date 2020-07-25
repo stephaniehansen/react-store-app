ccc1b96973a7fb687b1699d76677db643060e07c
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 00:13:06 +0100

    chore: update sample images

--- src/data/products.js
+++ src/data/products.js
@@ -4,28 +4,28 @@ const products = [
     name: "Product 1",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://picsum.photos/200/300"
+    img: "https://images.unsplash.com/photo-1587847615159-4c68b6b2da2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
   },
   {
     id: "02",
     name: "Product 2",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://picsum.photos/200/300"
+    img: "https://images.unsplash.com/photo-1585730385904-608936a0593f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
   },
   {
     id: "03",
     name: "Product 3",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://picsum.photos/200/300"
+    img: "https://images.unsplash.com/photo-1587155891445-d7d54cb09667?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
   },
   {
     id: "04",
     name: "Product 4",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://picsum.photos/200/300"
+    img: "https://images.unsplash.com/photo-1583176741168-d3c2b90ddbd4?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
   },
 ]
 
