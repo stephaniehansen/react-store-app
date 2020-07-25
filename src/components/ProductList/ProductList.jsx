3bd259b0dfee08f88e0fe9ead3d191ae5ddcd88a
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 22:35:02 +0100

    feat: display products from sample data

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import Product from "../Product"
+import styles from "./ProductList.module.scss";
+import Product from "../Product";
 
 export default class ProductList extends Component {
-  render() { 
+  getProducts = () => {
+    const { products } = this.props;
+    return products.map(product => <Product product={product}/> )
+  }
+
+  render() {
     return (
-      <>
-        <h2>Product List</h2>
-        <Product />
-      </>
+      <section className={styles.productList}>
+        {this.getProducts()}
+      </section>
     );
   }
 }
