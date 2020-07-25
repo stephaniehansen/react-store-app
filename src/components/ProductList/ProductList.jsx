e862e76d226c7a1a2668c4c24d28bf9fbe0fdb7f
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 20:38:57 +0100

    feat: add ProductList component

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() { 
+    return (
+      <h2>Product List</h2>
+    );
+  }
+}
+ 
+export default ProductList;
