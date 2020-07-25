bcaa73b92799c6ae50c0b740c7c7d28d88140e08
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 22:35:26 +0100

    feat: add basic  structure

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import styles from "./Product.module.scss";
 
 export default class Product extends Component {
   render() { 
+    const { name, img } = this.props.product;
+    
     return (
-      <h3>Product</h3>
+      <div className={styles.product}>
+        <img src={img} alt={name}/>
+        <h2>{name}</h2>
+      </div>
     );
   }
 }
