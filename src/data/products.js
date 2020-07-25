28ec6867bafeb83a72658853f3313f9aa6cd15aa
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-25 22:17:11 +0100

    chore: add product id

--- src/data/products.js
+++ src/data/products.js
@@ -1,23 +1,27 @@
 const products = [
   {
+    id: "01",
     name: "Product 1",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
     img: "https://picsum.photos/200/300"
   },
   {
+    id: "02",
     name: "Product 2",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
     img: "https://picsum.photos/200/300"
   },
   {
+    id: "03",
     name: "Product 3",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
     img: "https://picsum.photos/200/300"
   },
   {
+    id: "04",
     name: "Product 4",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
