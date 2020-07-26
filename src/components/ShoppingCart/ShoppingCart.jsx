4bab41dd5cfd4081dd4cbb3fa3debba3a3763e07
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 19:23:39 +0100

    chore: rename component

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Cart extends Component {
+export default class ShoppingCart extends Component {
   render() { 
     return ( 
       <h1>Shopping Cart</h1>
