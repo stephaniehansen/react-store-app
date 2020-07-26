b92668bd410b4dfe78246b7da5ffa07e1305b534
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 02:34:52 +0100

    chore: update data file

--- src/data/products.js
+++ src/data/products.js
@@ -4,28 +4,36 @@ const products = [
     name: "Product 1",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://images.unsplash.com/photo-1587847615159-4c68b6b2da2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    img: "https://images.unsplash.com/photo-1587847615159-4c68b6b2da2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    inCart: false,
+    newArrival: false
   },
   {
     id: "02",
     name: "Product 2",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://images.unsplash.com/photo-1585730385904-608936a0593f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    img: "https://images.unsplash.com/photo-1585730385904-608936a0593f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    inCart: false,
+    newArrival: true
   },
   {
     id: "03",
     name: "Product 3",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://images.unsplash.com/photo-1587155891445-d7d54cb09667?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    img: "https://images.unsplash.com/photo-1587155891445-d7d54cb09667?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    inCart: false,
+    newArrival: false
   },
   {
     id: "04",
     name: "Product 4",
     price: 9.99,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nostrum excepturi aliquid tenetur cumque et aliquam quia.",
-    img: "https://images.unsplash.com/photo-1593784820143-22a23474f3b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    img: "https://images.unsplash.com/photo-1593784820143-22a23474f3b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    inCart: false,
+    newArrival: false
   },
 ]
 
