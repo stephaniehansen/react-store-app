0d6604fccd4a6e747e3c75eb3e8b425671a8815d
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 19:17:03 +0100

    feat: add update quantity

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Router } from '@reach/router';
 
-import Cart from "../../components/Cart";
+import ShoppingCart from "../../components/ShoppingCart";
 import ProductList from "../../components/ProductList";
 
 export default class Routes extends Component {
   render() { 
-    const { products, addToCart } = this.props;
+    const { products, addToCart, updateQuantity } = this.props;
     return (
       <Router>
-        <ProductList path="/" products={products} addToCart={addToCart} />
-        <Cart path="cart" />
+        <ProductList path="/" products={products} addToCart={addToCart} updateQuantity={updateQuantity} />
+        <ShoppingCart path="cart" products={products}/>
       </Router>
      );
   }
