bb2b3985a9162a6ed63850136a1480506f180aeb
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 03:58:43 +0100

    feat: add cart icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import styles from "./NavBar.module.scss"
+import styles from "./NavBar.module.scss";
+
+import CartIcon from 'react-ionicons/lib/MdCart';
 
 class NavBar extends Component {
   render() { 
     return ( 
       <nav className={styles.navBar}>
-        Nav bar
-      </nav>
+        <span class={styles.logo}>
+          B<span>o</span>tanicals
+        </span>
+        <CartIcon />
+        </nav>
     );
   }
 }
