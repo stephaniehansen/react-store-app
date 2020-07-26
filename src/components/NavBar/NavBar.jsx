69274182981b70c76089af97d53cf7527009f0c2
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-26 04:26:45 +0100

    feat: add home and cart links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from "@reach/router";
 import styles from "./NavBar.module.scss";
 
 import CartIcon from 'react-ionicons/lib/MdCart';
 
-class NavBar extends Component {
+export default class NavBar extends Component {
   render() { 
     return ( 
       <nav className={styles.navBar}>
-        <span class={styles.logo}>
-          B<span>o</span>tanicals
-        </span>
-        <CartIcon />
+        <Link to="/">
+          <span class={styles.logo}>
+            B<span>o</span>tanicals
+          </span>
+        </Link>
+        <Link to="cart">
+          <CartIcon />
+        </Link>
         </nav>
     );
   }
 }
- 
-export default NavBar;
