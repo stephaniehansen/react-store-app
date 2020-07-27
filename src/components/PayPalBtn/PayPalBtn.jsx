d5315019c676b0325ab9d08e24d1df6c678121f3
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-28 00:37:21 +0100

    feat: empty cart after payment

--- src/components/PayPalBtn/PayPalBtn.jsx
+++ src/components/PayPalBtn/PayPalBtn.jsx
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { PayPalButton } from "react-paypal-button-v2";
 
 export default class PayPalBtn extends Component {
+  completeTransaction = () => {
+    this.props.emptyCart();
+    this.props.refreshCart(this.props.refreshCart);
+  }
 
   render() {
-    const { amount, completeTransaction } = this.props;
+    const { amount } = this.props;
       return ( 
-        <PayPalButton amount={amount} />
-   ); 
+        <PayPalButton
+          amount={amount}
+          onSuccess={(details) => {
+            alert(`Thank you for your purchase, ${details.payer.name.given_name}!`)
+            this.completeTransaction();
+          }}
+        />
+      ); 
   }
 }
