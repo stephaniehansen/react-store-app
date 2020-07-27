6fb093537f1b5c58a2488c216b618a7c9a793c98
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-27 17:44:27 +0100

    feat: display and update items in cart

--- src/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import styles from "./ShoppingCartItem.module.scss";
 
 export default class ShoppingCartItem extends Component {
+  state = {
+    inCart: this.props.product.inCart,
+    quantity: this.props.product.quantity
+  }
+
+  removeFromCart = () => {
+    this.props.addToCart(this.props.product);
+    this.props.updateCart(this.props.updateCart);
+    this.setState({ inCart: !this.state.inCart })
+  }
+
+  updateQuantity = (amount) => {
+    this.setState({ quantity: (this.state.quantity += amount) })
+    this.props.updateQuantity(this.props.product, this.state.quantity);
+  }
+
   render() { 
+    const { img, name, quantity, price } = this.props.product;
+    
     return (
-      <p>Shopping Cart Item</p>
+      <div className={styles.shoppingCartItem}>
+        <div className={styles.itemDetailsContainer}>
+          <div className={styles.itemImg}>
+            <img src={img} alt={name}/>
+          </div>
+          <div className={styles.itemInfo}>
+            <span><h2>{name}</h2></span>
+          </div>
+        </div>
+        <div className={styles.itemQuantity}>
+          <button onClick={()=> this.updateQuantity(-1)} disabled={this.state.quantity < 2 }>-</button>
+            <span>{quantity}</span>
+          <button onClick={()=> this.updateQuantity(1)}>+</button>
+        </div>
+        <div className={styles.itemPrice}>
+          £{price}
+        </div>
+        <div className={styles.itemRemove}>
+          <button onClick={this.removeFromCart}>X</button>
+        </div>
+      </div>
     );
   }
 }
