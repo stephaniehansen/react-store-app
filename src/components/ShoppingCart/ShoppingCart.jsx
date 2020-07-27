96e176030b38b006db809543f239f47aed6eb010
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-27 17:40:53 +0100

    feat: display and update items in cart

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import styles from "./ShoppingCart.module.scss";
+import ShoppingCartItem from "../ShoppingCartItem"
 
 export default class ShoppingCart extends Component {
-  render() { 
-    return ( 
-      <h1>Shopping Cart</h1>
+  state = {
+    cart: this.props.products.filter(product => product.inCart)
+  }
+
+  getCartItems = () => {
+    return this.state.cart
+    .map(item => <ShoppingCartItem product={item} addToCart={this.props.addToCart} updateQuantity={this.props.updateQuantity} removeFromCart={this.props.removeFromCart} updateCart={this.updateCart}/>);
+  }
+
+  getCartTotal = () => {
+    const itemPrices = this.state.cart.map(item => item.total);
+    return itemPrices.reduce((total, amount) => total + amount, 0).toFixed(2);
+  }
+
+  updateCart = () => {
+    this.setState({cart: this.props.products.filter(product => product.inCart)})
+  }
+
+  render() {
+    return (
+      <section className={styles.shoppingCart}>
+        <div className={styles.cartContainer}>
+          <div className={styles.leftPanel}>
+            <h1 className={styles.cartTitle}>Shopping Cart</h1>
+            <div className={styles.shoppingCartItems}>
+              {this.state.cart.length ? this.getCartItems() : <p>Your cart is empty.</p>}
+            </div>
+          </div>
+          <div className={styles.rightPanel}>
+            <div className={styles.summaryContainer}>
+              <h2>Order Summary</h2>
+              <div>
+                <span><h3>Total</h3></span>
+                <span><h3>£{this.getCartTotal()}</h3></span>
+              </div>
+              <button className={styles.checkoutBtn}>Checkout</button>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
