0fd7702ce3af64d102648e9d5186181d640fbe05
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 22:33:46 +0100

    feat: add hero structure - content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,19 @@ import styles from "./Hero.module.scss"
 export default class Hero extends Component {
   render() { 
     return (
-      <section className={styles.hero}>
-        <h1>Hero</h1>
-      </section>
+        <section className={styles.hero}>
+          <div className={styles.heroContainer}>
+            <div className={styles.heroContent}>
+              <div className={styles.heroTitle}>
+                <h1>Nourish your Skin</h1>
+              </div>
+              <div className={styles.heroDetails}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing. Tenetur repellendus, pariatur non quia possimus consequuntur alias natus atque, minima quis.</p>
+                <button>Shop Now</button>
+              </div>
+            </div>
+          </div>
+        </section>
      );
   }
 }
