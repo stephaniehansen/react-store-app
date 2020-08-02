f6f68a33a0f405ef9f0831fbb8d46b9399cbc517
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 21:27:33 +0100

    feat: update to main component

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Router } from '@reach/router';
 
+import Main from "../../components/Main";
 import ShoppingCart from "../../components/ShoppingCart";
-import ProductList from "../../components/ProductList";
 
 export default class Routes extends Component {
   render() { 
     const { products, updateCart, updateQuantity, emptyCart } = this.props;
     return (
       <Router>
-        <ProductList path="/" products={products} updateCart={updateCart} updateQuantity={updateQuantity} />
+        <Main path="/" products={products} updateCart={updateCart} updateQuantity={updateQuantity} />
         <ShoppingCart path="cart" products={products} updateCart={updateCart} updateQuantity={updateQuantity} emptyCart={emptyCart} />
       </Router>
      );
