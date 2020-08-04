47f8241a2dec2f14492a3ac04add544c06ff4cd7
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-04 04:47:28 +0100

    Updates

---
+++ precache-manifest.30cbdcaf908a644eb28ce9f3752e7f89.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89337eb91629ce338824d928ff88e81",
+    "url": "/react-store-app/index.html"
+  },
+  {
+    "revision": "6a4cc5351dfbefa07989",
+    "url": "/react-store-app/static/css/main.34bce259.chunk.css"
+  },
+  {
+    "revision": "aaa800bff686353bf95b",
+    "url": "/react-store-app/static/js/2.9d969579.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/react-store-app/static/js/2.9d969579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4cc5351dfbefa07989",
+    "url": "/react-store-app/static/js/main.473715cd.chunk.js"
+  },
+  {
+    "revision": "e470f02000a6d0bb3aa5",
+    "url": "/react-store-app/static/js/runtime-main.28b68be5.js"
+  },
+  {
+    "revision": "4b2f8e378a776343efc359dcbbb755bd",
+    "url": "/react-store-app/static/media/botanicals-banner.4b2f8e37.jpg"
+  },
+  {
+    "revision": "6b04e505d206826df5af0b6ef4362add",
+    "url": "/react-store-app/static/media/botanicals-hero.6b04e505.jpg"
+  },
+  {
+    "revision": "158d898d07bca8555023ccd6736fa483",
+    "url": "/react-store-app/static/media/product01.158d898d.jpg"
+  },
+  {
+    "revision": "6059086b72abf50b8dc1b55f0bb92c59",
+    "url": "/react-store-app/static/media/product02.6059086b.jpg"
+  },
+  {
+    "revision": "0fb01e25a3dac0ee93e004643f5057f3",
+    "url": "/react-store-app/static/media/product03.0fb01e25.jpg"
+  },
+  {
+    "revision": "9f6809ec5409cde2710401489481eece",
+    "url": "/react-store-app/static/media/product04.9f6809ec.jpg"
+  },
+  {
+    "revision": "af06fd90570a19c5ea5a41a26fdcb6e9",
+    "url": "/react-store-app/static/media/shopping-bag-icon.af06fd90.svg"
+  }
+]);
