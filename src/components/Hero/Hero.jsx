8bf7bc2b23dd89bf10303ef913aeaf5615e73123
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-04 01:01:37 +0100

    feat: update hero structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,17 +5,18 @@ export default class Hero extends Component {
   render() { 
     return (
         <section className={styles.hero}>
-          <div className={styles.heroContainer}>
-            <div className={styles.heroContent}>
-              <div className={styles.heroTitle}>
-                <h1>Nourish your Skin</h1>
-              </div>
-              <div className={styles.heroDetails}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing. Tenetur repellendus, pariatur non quia possimus consequuntur alias natus atque, minima quis.</p>
-                <button>Shop Now</button>
-              </div>
+          <div className={styles.heroImageWrapper}>
+          </div>
+          <div className={styles.heroContent}>
+            <div className={styles.heroTitle}>
+              <h1>Nourish your Skin</h1>
+            </div>
+            <div className={styles.heroDetails}>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing. Tenetur repellendus, pariatur non quia possimus consequuntur alias natus atque, minima quis.</p>
+              <button>Shop Now</button>
             </div>
           </div>
+
         </section>
      );
   }
